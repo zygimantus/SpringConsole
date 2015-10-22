bb06b68c05d14219a78aced15a8835ec14d41545
zygimantus <zygimantus@gmail.com>
2015-10-23 00:05:22 +0300

    pom changed, logger added

--- src/main/java/com/zygimantus/springconsole/App.java
+++ src/main/java/com/zygimantus/springconsole/App.java
@@ -1,5 +1,7 @@
 package com.zygimantus.springconsole;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,11 +11,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App {
 
+    private static final Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
 
         Message message = (Message) context.getBean("message");
 
-        System.out.println(message.getText());
+        logger.info("Message is: {}", message.getText());
     }
 }
